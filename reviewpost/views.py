b487a955529fd98d32482a932b286316174ad466
userhub2021 <taka021gmail.com>
2022-01-11 05:28:45 +0000

    更新

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from re import template
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from .models import ReviewModel
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def signupview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html',{'messege':'このユーザーは既に登録されています。'})    
+    else:
+        return render(request, 'signup.html', {})    
+    return render(request, 'signup.html', {'messege':'登録されました'})   
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        User = authenticate(request, username=username_data, password=password_data)
+        if User is not None:
+            login(request, User)
+            return redirect('list')
+    else:
+        return render(request, 'login.html')
+    return render(request, 'login.html')    
+
+def sampleview(request):
+    if request == 'POST':
+        return redirect('login')
+    else:
+        return render(request, 'login,html', {})    
+
+@login_required
+def listview(request):
+    object_list = ReviewModel.objects.all()
+    return render(request, 'list.html',{'object_list':object_list})
+
+def detailview(request, pk):
+    object = ReviewModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object':object})
+
+class CreateClass(CreateView):
+    template_name = 'create.html'
+    model = ReviewModel
+    fields = ('title', 'content', 'author', 'images', 'evaluation')
+    success_url = reverse_lazy('list')
+
+def logoutview(request):
+    logout(request)
+    return redirect('login')    
+
+def evaluationview(request, pk):
+    post = ReviewModel.objects.get(pk=pk)
+    author_name = request.user.get_username() + str(request.user.id)
+    if author_name in post.useful_review_record:
+        return redirect('list')
+    else:
+        post.useful_review = post.useful_review + 1
+        post.useful_review_record = post.useful_review_record + author_name
+        post.save()
+        return redirect('list')        
--- reviewproject/settings.py
+++ reviewproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviewpost.apps.ReviewpostConfig'
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'reviewproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR/'static')]
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/image/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
